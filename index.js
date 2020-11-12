8e35f9dcb5ae166addca9639086111b57f962604
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-13 09:26:16 +1100

    Initialized repo

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+// impliment the endpoint to watch for incoming events
+app.post("/events", (req, res) => {
+  //whatever comin gin the req is our event, we dont know what is that
+  const event = req.body;
+
+  //make post requests to different servers
+  axios.post("http://localhost:4000/events", event); // posts
+  axios.post("http://localhost:4001/events", event); // comments
+  axios.post("http://localhost:4002/events", event); // query service
+
+  // anytime anyone tries to send an event we send ok msg
+  res.send({ status: "OK" });
+});
+
+// event bus listening port
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
