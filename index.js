54638cb051e06c39deddbbd1e86749b1d3ea4baa
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-14 07:36:02 +1100

    Added async await to the endpoint requests

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 
 // impliment the endpoint to watch for incoming events
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   //whatever comin gin the req is our event, we dont know what is that
   const event = req.body;
 
   //make post requests to different servers
-  axios.post("http://localhost:4000/events", event); // posts
-  axios.post("http://localhost:4001/events", event); // comments
-  axios.post("http://localhost:4002/events", event); // query service
-  axios.post("http://localhost:4003/events", event); // comment moderation service
+  await axios.post("http://localhost:4000/events", event); // posts
+  await axios.post("http://localhost:4001/events", event); // comments
+  await axios.post("http://localhost:4002/events", event); // query service
+  await axios.post("http://localhost:4003/events", event); // comment moderation service
 
   // anytime anyone tries to send an event we send ok msg
   res.send({ status: "OK" });
