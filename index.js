3487a27ad6d3991311c02edd35da0f6f046651b8
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-15 05:22:01 +1100

    removed async await, cause it was giving error of unhandledrejection, not sure why,but it was gone when i removed async await, still not sure why?

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.use(bodyParser.json());
 const events = [];
 
 // impliment the endpoint to watch for incoming events
-app.post("/events", async (req, res) => {
+app.post("/events", (req, res) => {
   //whatever comin gin the req is our event, we dont know what is that
   const event = req.body;
 
   events.push(event);
 
   //make post requests to different servers
-  await axios.post("http://localhost:4000/events", event); // posts
-  await axios.post("http://localhost:4001/events", event); // comments
-  await axios.post("http://localhost:4002/events", event); // query service
-  await axios.post("http://localhost:4003/events", event); // comment moderation service
+  axios.post("http://localhost:4000/events", event); // posts
+  axios.post("http://localhost:4001/events", event); // comments
+  axios.post("http://localhost:4002/events", event); // query service
+  axios.post("http://localhost:4003/events", event); // comment moderation service
 
   // anytime anyone tries to send an event we send ok msg
   res.send({ status: "OK" });
